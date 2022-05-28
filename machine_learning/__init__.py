653c7a92c1859e5a968bf4bcbab9dcb8a63a7304
EunjaeYong <eunjae.yong@dslab.global>
2022-05-28 13:53:33 +0900

    library 정보 및 버전 변수 추가

---
+++ machine_learning/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.0.2"
+
+__library_info__ = {
+        "author": "EunJae Yong (Jay Yong)",
+        "version": "0.0.2",
+        "description": "This library was created for students studying autoML.",
+        "python_requires": ">=3.6"
+    }
